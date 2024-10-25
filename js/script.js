d2c2b5f58dcd4e2c378ab98d8dd3b02f6a0fdf1c
danturnerxyz <d4nielturner@gmail.com>
2024-10-25 14:06:29 +0100

    Pagination feature complete

--- js/script.js
+++ js/script.js
@@ -263,7 +263,13 @@ async function search() {
   }
 }
 
+// Display search results
 function displaySearchResults(results) {
+
+  // Clear previous results
+  document.querySelector('#search-results').innerHTML = '';
+  document.querySelector('#search-results-heading').innerHTML = '';
+  document.querySelector('#pagination').innerHTML = '';
 
   results.forEach(result => {
     const div = document.createElement('div');
@@ -275,18 +281,18 @@ function displaySearchResults(results) {
               ? `<img
               src="https://image.tmdb.org/t/p/w500/${result.poster_path}"
               class="card-img-top"
-              alt="${global.search.type === 'movie' ? result.title : results.name}"
+              alt="${global.search.type === 'movie' ? result.title : result.name}"
             />` : `<img
               src="images/no-image.jpg"
               class="card-img-top"
-              alt="${global.search.type === 'movie' ? result.title : results.name}"
+              alt="${global.search.type === 'movie' ? result.title : result.name}"
             />`
             }
           </a>
           <div class="card-body">
-            <h5 class="card-title">${global.search.type === 'movie' ? result.title : results.name}</h5>
+            <h5 class="card-title">${global.search.type === 'movie' ? result.title : result.name}</h5>
             <p class="card-text">
-              <small class="text-muted">Release: ${global.search.type === 'movie' ? result.release_date : results.first_air_date}</small>
+              <small class="text-muted">Release: ${global.search.type === 'movie' ? result.release_date : result.first_air_date}</small>
             </p>
           </div>
         `;
@@ -297,7 +303,47 @@ function displaySearchResults(results) {
     `
 
     document.querySelector('#search-results').appendChild(div);
+
   });
+
+  displayPagination() 
+}
+
+// Create & display pagination
+function displayPagination() {
+  const div = document.createElement('div')
+  div.classList.add('pagination')
+  div.innerHTML = `
+  <button class="btn btn-primary" id="prev">Prev</button>
+  <button class="btn btn-primary" id="next">Next</button>
+  <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>
+  `
+
+  document.querySelector('#pagination').appendChild(div)
+
+  // Disable prev button if on first page
+  if (global.search.page === 1) {
+    document.querySelector('#prev').disabled = true;
+  }
+
+  // Disable next button if on last page
+  if (global.search.page === global.search.totalPages) {
+    document.querySelector('#next').disabled = true;
+  }
+
+  // Next page
+  document.querySelector('#next').addEventListener('click', async () => {
+    global.search.page++;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results)
+  })
+
+  // Prev page
+  document.querySelector('#prev').addEventListener('click', async () => {
+    global.search.page--;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results)
+  })
 }
 
 // Display slider movies
@@ -369,7 +415,7 @@ async function searchAPIData() {
 
   showSpinner();
 
-  const response = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`)
+  const response = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`)
 
   const data = await response.json();
 
